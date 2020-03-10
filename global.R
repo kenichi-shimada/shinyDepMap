89b29238ec2d71e5a1646a0065ae38b614268fc8
kenichi-shimada <kenichi_shimada@hms.harvard.edu>
2020-03-10 15:02:27 -0400

    update global.R

--- global.R
+++ global.R
@@ -17,12 +17,3 @@ source("modules/essentiality.R",local=TRUE)
 source("modules/cluster.R",local=TRUE)
 
 x <- load("data/depmap_initial_19q3_local_run.rda")
-
-##
-if(0){
-	# install.packages("aws.signature", repos = c("cloudyr" = "http://cloudyr.github.io/drat"))
-	# install.packages("aws.s3", repos = c("cloudyr" = "http://cloudyr.github.io/drat"))
-	library(shiny)
-	setwd("~/Dropbox (HMS)/projects/shinyapps/shinyDepMap/")
-	runApp("depmap-local")
-}
